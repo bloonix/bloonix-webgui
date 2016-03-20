bdb085dbfeda9e13b103ea3a344a46540fce4d3f
Jonny Schulz <support@bloonix.de>
2016-03-20 15:32:34 +0100

    kicked host-reg-example

--- host-reg-example.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -H 'Content-Type: application/json' 'https://gui.bloonix.de/register/host?pretty' -d '{
-    "company_id": 1,
-    "company_authkey": "secret",
-    "hostname": "test.bloonix.de",
-    "ipaddr": "127.0.0.1"
-}'
