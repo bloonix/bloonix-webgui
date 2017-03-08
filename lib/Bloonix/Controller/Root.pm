4e34a5f31fd61b301fe2819a4a8f5402fb08a359
Jonny Schulz <support@bloonix.de>
2017-03-08 23:21:31 +0100

    version upgraded

--- lib/Bloonix/Controller/Root.pm
+++ lib/Bloonix/Controller/Root.pm
@@ -7,7 +7,7 @@ use Time::HiRes;
 sub auto {
     my ($self, $c) = @_;
 
-    $c->version->{js} = 134;
+    $c->version->{js} = 135;
 
     my $addr = $c->req->remote_addr || "n/a";
     my $lang = $c->req->cookie("lang");
